1e027c355d09d5fd621a9bfc9831537775c1c77e
Joseph Smith <jsmith47265@gmail.com>
2024-08-23 13:06:24 -0400

    Initial commit with Next.js and Chakra UI setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Box textAlign="center" mt={10}>
+      <Heading>Welcome to the Quiz and Flashcard App</Heading>
+    </Box>
+  );
+}
