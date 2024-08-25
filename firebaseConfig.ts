9a78f3944942c0659a9f064c144324cb9541aa56
Joseph Smith <jsmith47265@gmail.com>
2024-08-25 08:53:09 -0400

    remove keys

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2pnHWx-Raivc9zBu4PQG0lWdDeUsF6eM",
-    authDomain: "bruteforce-study-app.firebaseapp.com",
-    projectId: "bruteforce-study-app",
-    storageBucket: "bruteforce-study-app.appspot.com",
-    messagingSenderId: "925399018045",
-    appId: "1:925399018045:web:5d1b39f1b3ed6e0f045854",
-    measurementId: "G-R2MQKQNS39"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
