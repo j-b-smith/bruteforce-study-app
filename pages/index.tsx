9a28074cbfd658a943bcf34296373a32e51b356d
Joseph Smith <jsmith47265@gmail.com>
2024-10-05 09:02:11 -0400

    height

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
     <Box
-      height="100vh"
+      height="90vh"
       display="flex"
       justifyContent="center"
       alignItems="center"
